4bc7f4e2a412a2dce88dd4fce55e0c3e2105a5e3
feifei-111 <2364819892@qq.com>
2025-10-04 02:01:33 +0800

    update

--- gltest/windows_main.cc
+++ gltest/windows_main.cc
@@ -11,88 +11,7 @@ int main(){
     // fg_utils::CreateDebugConsole();
     auto window = fg_window::Window(L"Game", SCR_WIDTH, SCR_HEIGHT);
 
-    fg_gl::ShaderProgram program("./assets/shaders/vertex.glsl", "./assets/shaders/fragment.glsl");
-
-
-    float vertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    };
-
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 0.0f,  0.0f,  0.0f), 
-        glm::vec3( 2.0f,  5.0f, -15.0f), 
-        glm::vec3(-1.5f, -2.2f, -2.5f),  
-        glm::vec3(-3.8f, -2.0f, -12.3f),  
-        glm::vec3( 2.4f, -0.4f, -3.5f),  
-        glm::vec3(-1.7f,  3.0f, -7.5f),  
-        glm::vec3( 1.3f, -2.0f, -2.5f),  
-        glm::vec3( 1.5f,  2.0f, -2.5f), 
-        glm::vec3( 1.5f,  0.2f, -1.5f), 
-        glm::vec3(-1.3f,  1.0f, -1.5f)  
-    };
-
-    glEnable(GL_DEPTH_TEST);  
-
-    fg_gl::VAO vao;
-    fg_gl::VBO<float> vbo(vertices, 5, 36, GL_FLOAT, GL_STATIC_DRAW, 0);
-
-    vbo.SetAttr(3, GL_FALSE);
-    vbo.SetAttr(2, GL_FALSE);
-    vao.UnBind();
-    vbo.UnBind();
-
-    fg_gl::Texture2D tex0("./assets/textures/awesomeface.png", 0, GL_RGB, GL_RGBA);
-    fg_gl::Texture2D tex1("./assets/textures/wall.jpg", 3, GL_RGB, GL_RGB);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-
-    // glm::mat4 trans{1.0};
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, -1.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));  
-    glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
-
+    Render render;
 
     fg_interact::Event event;
     while(true){
@@ -103,34 +22,11 @@ int main(){
         }
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 这里，如果没有 GL_DEPTH_BUFFER_BIT 会导致无法正常显示，
+        // 我以为只会导致 zbuffer 没清空，使得显示错位，没想到直接就不输出了，离谱，害我 debug 半天
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // draw our first triangle
-        vao.Bind();
-        program.Use();
-        program.UniformInt("Tex0", tex0.CurrentUnit());
-        program.UniformInt("Tex1", tex1.CurrentUnit());
-
-        float timeValue = fg_utils::GetTime();
-        const float radius = 10.0f;
-        float camX = sin(timeValue) * radius;
-        float camZ = cos(timeValue) * radius;
-
-        glm::mat4 view;
-        glm::mat4 projection;
-        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));  
-        projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
-
-        for (int i=0; i<10; i++){
-            glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, cubePositions[i]);
-            model = glm::rotate(model, timeValue * (i+1), glm::vec3(0.5f, 1.0f, 0.0f));
-            glm::mat4 trans = projection * view * model;
-            program.UniformFloatMat4Vec("trans", glm::value_ptr(trans));
-            std::cout << "glDrawArrays" << std::endl;
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-
+        render.Draw();
         window.SwapBuffer();
 
     }
